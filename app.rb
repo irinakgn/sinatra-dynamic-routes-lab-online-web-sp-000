138e02143bbe9191093c01504144ee39dc8f8ee1
Irina Kagan <irinagamb@gmail.com>
2019-07-29 13:29:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ end
  get "/say/:number/:phrase" do
    @number = params[:number].to_i
    @phrase = params[:phrase]
-   "#{@phrase}\n"*"#{@number}"
+   "#{@phrase}\n"*"@number"
  end
 
 get "/say/:word1/:word2/:word3/:word4/:word5" do
