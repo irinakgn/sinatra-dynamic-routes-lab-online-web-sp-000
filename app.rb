4e8239aecb6a3de4b05795322e72c72a83cdcefb
Irina Kagan <irinagamb@gmail.com>
2019-07-29 13:36:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,19 +18,14 @@ end
  end
 
 get "/say/:word1/:word2/:word3/:word4/:word5" do
-      @word1 = params [:word1]
-      @word2 = params [:word2]
-      @word3 = params [:word3]
-      @word4 = params [:word4]
-      @word5 = params [:word5]
-    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
 end
 
 get "/:operation/:number1/:number2" do
   @number1 = params[:number1].to_i
   @number2 =params[:number2].to_i
   @operation = params[:operation]
-  "#{@number1} #{@operation} #{@number2}"
+  @number1.send(@operation,number2)
 
 end
 end
