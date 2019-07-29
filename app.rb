a59fe8acd0f977a9c6a071e33f50d3c35e294600
Irina Kagan <irinagamb@gmail.com>
2019-07-29 13:13:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
  get "/square/:number" do
    @number = params[:number].to_i
     "#{@number*@number}"
-    
+
 end
  get "/say/:number/:phrase" do
    @number = params[:number].to_i
@@ -25,5 +25,5 @@ get "/say/:word1/:word2/:word3/:word4/:word5"
       @word5 = params [:word5]
       "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
 
- 
+
 end
